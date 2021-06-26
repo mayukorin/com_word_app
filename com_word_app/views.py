20916b003cff4931a0a7b930b76023256ad4a82e
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-06-26 18:20:50 +0900

    Add nltk function

--- com_word_app/views.py
+++ com_word_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework import  views, status
 from rest_framework.response import Response
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import nltk
 
 # Create your views here.
 
 class SentenceAnalyzeView(views.APIView):
 
     def post(self, request, *args, **kwargs):
-        print("okkk")
+        token = word_tokenize(request.data["sentence"])
+        print(request.data["sentence"])
+        print(token)
+        fd = nltk.FreqDist(token)
+        print(fd.most_common(10))
         return Response(status.HTTP_200_OK)
