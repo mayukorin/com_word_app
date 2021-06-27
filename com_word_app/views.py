67ea6b68963f21f2888b4e232b6a78e8d5a57b60
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-06-27 19:38:03 +0900

    Return nltk analysis

--- com_word_app/views.py
+++ com_word_app/views.py
@@ -11,8 +11,24 @@ class SentenceAnalyzeView(views.APIView):
 
     def post(self, request, *args, **kwargs):
         token = word_tokenize(request.data["sentence"])
-        print(request.data["sentence"])
-        print(token)
+        # print(request.data["sentence"])
+        # print(token)
         fd = nltk.FreqDist(token)
-        print(fd.most_common(10))
-        return Response(status.HTTP_200_OK)
+        fd = fd.most_common(10)
+        word_list = []
+        cnt_list = []
+        print(fd)
+        for f in fd:
+            print(f)
+            word_list.append(f[0])
+            cnt_list.append(f[1])
+        print(word_list)
+        print(cnt_list)
+        res_list = [
+            {
+                'words' : word_list,
+                'cnt' : cnt_list,
+            }
+
+        ]
+        return Response(res_list, status=status.HTTP_200_OK)
