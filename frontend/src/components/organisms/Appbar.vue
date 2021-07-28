8b2413d9dfa926650ea3e78d9618649ebbc991d4
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-07-29 00:07:38 +0900

    Add navbar

---
+++ frontend/src/components/organisms/Appbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar flat app>
+        <v-app-bar-nav-icon class="grey--text" />
+        <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">CommonWordSearch</span>
+        </v-toolbar-title>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    name: "Appbar"
+}
+</script>
---
+++ frontend/src/components/organisms/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <Appbar />
+    </nav>
+</template>
+<script>
+import Appbar from "@/components/organisms/Appbar";
+export default {
+    name: "Navbar",
+    component: {
+        Appbar
+    }
+}
+</script>
